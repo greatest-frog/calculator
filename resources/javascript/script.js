cc157e71366965c45d22d03119828342febaab0b
greatest-frog <110398760+greatest-frog@users.noreply.github.com>
2022-09-04 12:52:55 +0300

    Add the main page with content

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,3 +1,8 @@
+const result = document.getElementById('result');
+const active_buttons = document.querySelectorAll('.active_button');
+const buttons = document.querySelectorAll('.button');
+
+
 function add(a, b) {
     return a + b;
 }
@@ -12,4 +17,11 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function operate(a, b, operator) {
+    if (operator === '+') return add(a, b);
+    else if (operator === '-') return subtract(a, b);
+    else if (operator === '*') return multiply(a, b);
+    else if (operator === '/') return divide(a, b);
 }
