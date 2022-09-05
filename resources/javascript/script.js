8d14a7705917375db88896b4fde6c508ad4a237e
greatest-frog <110398760+greatest-frog@users.noreply.github.com>
2022-09-05 11:04:00 +0300

    Add functionality for Clear and Backspace buttons

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,7 +1,32 @@
-const result = document.getElementById('result');
-const active_buttons = document.querySelectorAll('.active_button');
-const buttons = document.querySelectorAll('.button');
+const DEFAULT_PREVIOUS_NUMBER = null;
+const DEFAULT_NUMBER = 0;
+const DEFAULT_OPERATOR = '';
 
+const result = document.getElementById('result');
+const clear = document.getElementById('clear');
+const backspace = document.getElementById('backspace');
+const number_buttons = document.querySelectorAll('.number');
+const operator_buttons = document.querySelectorAll('.operator');
+
+let previous_number = DEFAULT_PREVIOUS_NUMBER;
+let now_number = DEFAULT_NUMBER;
+let now_operator = DEFAULT_OPERATOR;
+
+clear.addEventListener('click', e => {
+    previous_number = null;
+    now_number = 0;
+    now_operator = '';
+    e.target.classList.add('clicked');
+    setTimeout(() => e.target.classList.remove('clicked'), 100)
+    displayResult();
+})
+
+backspace.addEventListener('click', (e) => {
+    now_number = (now_number - now_number % 10) / 10;
+    e.target.classList.add('clicked');
+    setTimeout(() => e.target.classList.remove('clicked'), 100)
+    displayResult();
+})
 
 function add(a, b) {
     return a + b;
@@ -25,3 +50,9 @@ function operate(a, b, operator) {
     else if (operator === '*') return multiply(a, b);
     else if (operator === '/') return divide(a, b);
 }
+
+function displayResult() {
+    result.textContent = now_operator + now_number;
+}
+
+displayResult();
